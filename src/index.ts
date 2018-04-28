b7e0470d8d7ad19d6f3a2687fc41f79ac2cd8501
Yu Chen <yu.chen@live.ie>
2018-04-28 12:44:49 +0800

    here

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,8 @@ export * from './component';
     RestAdminFieldComponent,
   ],
   exports: [
-    RestAdminComponent
+    RestAdminComponent,
+    RestAdminFieldComponent,
   ]
 })
 export class RestAdminModule {
